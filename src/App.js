800c0b8f5a273733441f2b8d592480fb658e88e7
Daniel Colgan <colgandm@tcd.ie>
2021-02-27 21:59:17 +0000

    Updating dummy project

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hiasd !</p>
-        <a 
+        <p>Bye there!</p>
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
